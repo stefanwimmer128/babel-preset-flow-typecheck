c5a7f7eba5150a12998b631c88fe7ce2262089f7
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-12-26 14:31:16 +0100

    Refactoring

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
  * @author Stefan Wimmer <stefanwimmer128@gmail.com>
  */
  
- module.exports = {
-     plugins: [
-         require("babel-plugin-syntax-flow"),
-         require("babel-plugin-transform-flow-strip-types"),
-         require("babel-plugin-typecheck"),
-     ],
- };
- 
+module.exports = {
+    plugins: [
+        require("babel-plugin-syntax-flow"),
+        require("babel-plugin-transform-flow-strip-types"),
+        require("babel-plugin-typecheck"),
+    ],
+};
