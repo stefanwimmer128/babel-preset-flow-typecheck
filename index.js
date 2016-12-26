c92a2e0897eb6bef2b7a9d35499c2b9430277f9e
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-12-26 14:26:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created on 26.12.16 at 14:23
+ * @author Stefan Wimmer <stefanwimmer128@gmail.com>
+ */
+ 
+ module.exports = {
+     plugins: [
+         require("babel-plugin-syntax-flow"),
+         require("babel-plugin-transform-flow-strip-types"),
+         require("babel-plugin-typecheck"),
+     ],
+ };
+ 
