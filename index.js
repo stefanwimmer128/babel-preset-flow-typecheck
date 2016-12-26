06b8d6c22af19513e4421dc90fa606476e7a272a
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-12-26 15:54:36 +0100

    #

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ module.exports = {
     plugins: [
         require("babel-plugin-syntax-flow"),
         require("babel-plugin-transform-flow-strip-types"),
-        require("babel-plugin-typecheck"),
+        require("babel-plugin-typecheck").default,
     ],
 };
